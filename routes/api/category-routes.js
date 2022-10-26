10e1200cfe0c4cefb945e32cd9c06f5c570a056e
Daniel Papert <dpapert@gmail.com>
2022-10-25 20:42:15 -0400

    Added methods to product and Sequelize models.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,13 +7,21 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
    // find all categories
     // be sure to include its associated Products
-    Category.findAll()
+    Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+            {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+            }
+            ]
+        })
         .then(categoryData => res.json(categoryData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
-});
+    });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
@@ -21,8 +29,14 @@ router.get('/:id', (req, res) => {
   Category.findOne({
     where: {
         id: req.params.id
-    }
-    })
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+    ]})
     .then(categoryData => {
         if (!categoryData) {
             res.status(404).json({ message: 'No category found with id of ' + id });
